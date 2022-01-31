49ad592337d9e6f75f6b2c70950e2e149fd0be63
HridoyAlam <optimus5289@gmail.com>
2022-01-31 17:27:18 +0600

    user input taken

--- dice_roller.py
+++ dice_roller.py
@@ -1,17 +1,23 @@
 import random
 def main():
-  dice_rolls = int(input("How many dice would you like to roll? "))
-  dice_sum = 0
-  for i in range(0,dice_rolls):
-    roll = random.randint(1, 6)
-    dice_sum +=roll
-    if roll == 1:
-      print(f'You rolled a {roll} ! Crictical Fail')
-    elif roll == 6:
-      print(f'You rolled {roll} ! Critical Success')
-    else:
-      print(f'You rolled {roll}')
-  print(f'You have rolled total of {dice_sum}')
+  num_player = int(input("\tHow many player wants to Play? "))
+  dice_rolls = int(input("\tHow many dice would you like to roll? "))
+
+  print("\n")
+
+  for j in range(0, num_player):
+    
+    dice_sum = 0
+    for i in range(0,dice_rolls):
+      roll = random.randint(1, 6)
+      dice_sum +=roll
+      if roll == 1:
+        print(f'\tYou rolled a {roll} ! Crictical Fail')
+      elif roll == 6:
+        print(f'\tYou rolled {roll} ! Critical Success')
+      else:
+        print(f'\tYou rolled {roll}')
+    print(f'\tPlayer {j+1}\t: have rolled total of {dice_sum}\n\n')
 
 if __name__== "__main__":
   main()
