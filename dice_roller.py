9b4f20468ef798de75e026b7075204a1aa5bda2d
HridoyAlam <optimus5289@gmail.com>
2022-01-31 17:13:55 +0600

    Condition Added

--- dice_roller.py
+++ dice_roller.py
@@ -5,7 +5,12 @@ def main():
   for i in range(0,dice_rolls):
     roll = random.randint(1, 6)
     dice_sum +=roll
-    print(f'You rolled a {roll}')
+    if roll == 1:
+      print(f'You rolled a {roll} ! Crictical Fail')
+    elif roll == 6:
+      print(f'You rolled {roll} ! Critical Success')
+    else:
+      print(f'You rolled {roll}')
   print(f'You have rolled total of {dice_sum}')
 
 if __name__== "__main__":
